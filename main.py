8c5d6c646e590f63936a2cdb59cc95ed5e4fdc54
aldsouza4@gmail.com <allwin47>
2020-12-14 03:28:19 +0530

    Fixed Bugs in make_prediction function

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import numpy as np
-global percentage
 
 
 def clean_values(num):
@@ -52,61 +51,6 @@ def naming_function(name_):
 def clean_ser(val):
     return (np.around((val.reshape(-1)), 2)).tolist()
 
-
-def make_predictions(input_list, column_list, num_terms_pred, name):
-
-    pd_index = naming_function(name_=name)
-    lm = LinearRegression()
-    index_input_list = np.arange(len(input_list))
-
-    input_list = np.array(input_list).reshape(-1, 1)
-
-    index_input_list = np.array(index_input_list).reshape(-1, 1)
-
-    lm.fit(X=index_input_list, y=input_list)
-
-    predict_me = np.arange(len(input_list), len(input_list) + num_terms_pred)
-
-    predict_me = predict_me.reshape(-1, 1)
-    predictions = lm.predict(predict_me)
-    predictions = clean_ser(predictions)
-    predict_me = clean_ser(predict_me)
-
-    index_input_list = clean_ser(index_input_list)
-    input_list = clean_ser(input_list)
-
-    predict_label = np.arange(len(input_list), len(input_list) + num_terms_pred)
-    predict_label = clean_ser(predict_label)
-
-    cont_predictions = predictions
-
-    predict_label.append(index_input_list[-1])
-    cont_predictions.append(input_list[-1])
-
-    # return predictions
-
-    scrap = int(column_list[-1][-2:]) + 1
-    for i in range(num_terms_pred):
-        column_list.append("March 20{} E".format(scrap + i))
-
-    plt.figure(figsize=(15, 8))
-    sns.lineplot(x=index_input_list, y=input_list, color='red')
-    sns.lineplot(x=predict_label, y=cont_predictions, color='green', linewidth=2)
-    plt.xticks(ticks=range(0, len(column_list)), labels=column_list, rotation='vertical', fontsize=8)
-    plt.xlabel('Results declared in', fontsize=14)
-    if percentage:
-        plt.ylabel('In %', fontsize=14)
-        plt.title("{}".format(pd_index), fontsize=18)
-        plt.show()
-
-    else:
-        plt.ylabel('INR in crores', fontsize=14)
-        plt.title("{}".format(pd_index), fontsize=18)
-        plt.show()
-
-    return input_list + predictions
-
-
 class FinancialData(object):
 
     def __init__(self):
@@ -128,12 +72,12 @@ class FinancialData(object):
 
         data.dropna(axis=0, inplace=True)
 
-        column_list = data.columns
-        column_list = column_list[1:]
-        column_list = column_list.tolist()
+        self.column_list = data.columns
+        self.column_list = self.column_list[1:]
+        self.column_list = self.column_list.tolist()
 
 
-        for i in column_list:
+        for i in self.column_list:
             data[i] = data[i].apply(clean_values)
 
         if display:
@@ -141,12 +85,9 @@ class FinancialData(object):
         else:
             return data
 
-    def get_results(self, table_name, name, as_list=False, plot_inner=False, asDataFrame=True):
+    def __get_results(self, table_name, name, as_list=False, plot_inner=False, asDataFrame=True):
         data = self.financial_results(name=table_name)
-        percentage = False
-        column_list = data.columns
-        column_list = column_list[1:]
-        column_list = column_list.tolist()
+        self.percentage = False
 
         if name == "quarter_revenue":
             row_index = 0
@@ -154,7 +95,7 @@ class FinancialData(object):
             row_index = 2
         elif name == "quarter_operating_margins":
             row_index = 3
-            percentage = True
+            self.percentage = True
         elif name == "quarter_profit_before_tax":
             row_index = 7
         elif name == "quarter_net_profit":
@@ -166,7 +107,7 @@ class FinancialData(object):
             row_index = 2
         elif name == "annual_operating_margins":
             row_index = 3
-            percentage = True
+            self.percentage = True
         elif name == "annual_profit_before_tax":
             row_index = 7
         elif name == "annual_net_profit":
@@ -189,9 +130,9 @@ class FinancialData(object):
 
             plt.figure(figsize=(15, 8))
             sns.lineplot(x=index_list, y=data)
-            plt.xticks(ticks=range(0, len(index_list)), labels=column_list, rotation='vertical', fontsize=8)
+            plt.xticks(ticks=range(0, len(index_list)), labels=self.column_list, rotation='vertical', fontsize=8)
             plt.xlabel('Results declared in', fontsize=14)
-            if percentage:
+            if self.percentage:
                 plt.ylabel('In %', fontsize=14)
                 plt.title("{}".format(pd_index), fontsize=18)
                 plt.show()
@@ -207,80 +148,135 @@ class FinancialData(object):
         if asDataFrame:
             pd_index = naming_function(name_=name)
 
-            data = pd.DataFrame(data=[data], columns=column_list, index=[pd_index])
+            data = pd.DataFrame(data=[data], columns=self.column_list, index=[pd_index])
             return data
 
     # -----------QUARTER DATA-----------------------------------------------------
 
     def quarter_revenue(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "quarter_revenue"
+        self.r_name = "quarter_revenue"
         t_name = "quarter"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def quarter_operating_profits(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "quarter_operating_profits"
+        self.r_name = "quarter_operating_profits"
         t_name = "quarter"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def quarter_operating_margins(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "quarter_operating_margins"
+        self.r_name = "quarter_operating_margins"
         t_name = "quarter"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def quarter_profit_before_tax(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "quarter_profit_before_tax"
+        self.r_name = "quarter_profit_before_tax"
         t_name = "quarter"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def quarter_net_profit(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "quarter_net_profit"
+        self.r_name = "quarter_net_profit"
         t_name = "quarter"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     # -----------ANNUAL DATA-----------------------------------------------------
 
     def annual_revenue(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "annual_revenue"
+        self.r_name = "annual_revenue"
         t_name = "annual"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def annual_operating_profits(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "annual_operating_profits"
+        self.r_name = "annual_operating_profits"
         t_name = "annual"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def annual_operating_margins(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "annual_operating_margins"
+        self.r_name = "annual_operating_margins"
         t_name = "annual"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def annual_profit_before_tax(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "annual_profit_before_tax"
+        self.r_name = "annual_profit_before_tax"
         t_name = "annual"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def annual_net_profit(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "annual_net_profit"
+        self.r_name = "annual_net_profit"
         t_name = "annual"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
 
     def annual_eps(self, as_list=False, plot=False, as_DataFrame=True):
-        r_name = "annual_eps"
+        self.r_name = "annual_eps"
         t_name = "annual"
-        return self.get_results(table_name=t_name, name=r_name, as_list=as_list, plot_inner=plot,
-                                asDataFrame=as_DataFrame)
+        return self.__get_results(table_name=t_name, name=self.r_name, as_list=as_list, plot_inner=plot,
+                                  asDataFrame=as_DataFrame)
+
+    def make_predictions(self, input_list, num_terms_pred):
+
+        name=self.r_name
+
+        pd_index = naming_function(name_=name)
+        lm = LinearRegression()
+        index_input_list = np.arange(len(input_list))
+
+        input_list = np.array(input_list).reshape(-1, 1)
+
+        index_input_list = np.array(index_input_list).reshape(-1, 1)
+
+        lm.fit(X=index_input_list, y=input_list)
+
+        predict_me = np.arange(len(input_list), len(input_list) + num_terms_pred)
+
+        predict_me = predict_me.reshape(-1, 1)
+        predictions = lm.predict(predict_me)
+        predictions = clean_ser(predictions)
+        predict_me = clean_ser(predict_me)
+
+        index_input_list = clean_ser(index_input_list)
+        input_list = clean_ser(input_list)
+
+        predict_label = np.arange(len(input_list), len(input_list) + num_terms_pred)
+        predict_label = clean_ser(predict_label)
+
+        cont_predictions = predictions
+
+        predict_label.append(index_input_list[-1])
+        cont_predictions.append(input_list[-1])
+
+        # return predictions
+
+        scrap = int(self.column_list[-1][-2:]) + 1
+        for i in range(num_terms_pred):
+            self.column_list.append("March 20{} E".format(scrap + i))
+
+        plt.figure(figsize=(15, 8))
+        sns.lineplot(x=index_input_list, y=input_list, color='red')
+        sns.lineplot(x=predict_label, y=cont_predictions, color='green', linewidth=2)
+        plt.xticks(ticks=range(0, len(self.column_list)), labels=self.column_list, rotation='vertical', fontsize=8)
+        plt.xlabel('Results declared in', fontsize=14)
+        if self.percentage:
+            plt.ylabel('In %', fontsize=14)
+            plt.title("{}".format(pd_index), fontsize=18)
+            plt.show()
+
+        else:
+            plt.ylabel('INR in crores', fontsize=14)
+            plt.title("{}".format(pd_index), fontsize=18)
+            plt.show()
+
+        return input_list + predictions
 
 
 t = FinancialData()
-print(t.annual_operating_margins(plot=True))
+print(t.make_predictions(t.annual_net_profit(as_list=True), 10))
 
