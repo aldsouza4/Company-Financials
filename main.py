a1a09dc03b164777ea469da37f84f7674f3df3ef
aldsouza4 <aldsouza4@gmail.com>
2021-03-05 20:59:49 +0300

    seperated the two files

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
-
 def clean_values(num):
     """
     takes in a string and returns the int or float or percentage (in decimal)
@@ -128,7 +127,6 @@ class FinancialData(object):
         2.Annual Profit and loss Statements
         3.Balance Sheet Items
         4.Cash Flow items
-
         :param self:
         :param name: takes in the table name
         :param display: True to display the tabel
@@ -156,8 +154,7 @@ class FinancialData(object):
             if display:
                 return data.to_string()
             else:
-                return data.drop('Mar 2020', axis=1)
-
+                return data
         elif name == "cash_flow":
             table_index = 7
             data = pd.DataFrame(self.df[table_index])
@@ -168,7 +165,7 @@ class FinancialData(object):
             if display:
                 return data.to_string()
             else:
-                return data.drop('Mar 2020', axis=1)
+                return data
 
         elif name == "ROCE":
             table_index = 8
@@ -187,20 +184,18 @@ class FinancialData(object):
         if display:
             return data.to_string()
         else:
-            return data.drop('Mar 2020', axis=1)
+            return data
 
     def __get_results(self, table_name, name, as_list=False, plot_inner=False, asDataFrame=True):
         """
         private method
         takes in the table item and returns it in the desired form
-
         :param table_name:takes in the name of the table
         :param name: name of the item from the list
         :param as_list: if True returns the the row as a list
         :param plot_inner: if True Plots a seaborn line plot
         :param asDataFrame: if True returns the the row as a Dataframe
         :return: returns the row in desired data type
-
         """
         data = self.financial_results(name=table_name)
         self.percentage = False
@@ -304,7 +299,6 @@ class FinancialData(object):
     def make_predictions(self, input_list, num_terms_pred=3, plot=False, as_list=False, asDataFrame=True,
                          only_prediction_list=False):
         """
-
         :param input_list: Takes in the input as list
         :param num_terms_pred: takes in the number of year to be predicted : default is 3 years
         :param plot: If True plots a seaborn line plot
@@ -374,12 +368,11 @@ class FinancialData(object):
             return predictions[-num_terms_pred:]
 
         if asDataFrame:
-            data = pd.DataFrame(data=[input_list + predictions[:-1]], columns=self.column_list[:-1], index=[self.pd_index])
+            data = pd.DataFrame(data=[input_list + predictions[:-1]], columns=self.column_list, index=[self.pd_index])
             return data
 
     def disp_data(self, data, as_list=False, plot=False, as_DataFrame=True, average=False):
         """
-
         :param data: takes in the data
         :param as_list: if True returns as list
         :param plot: if True returns a seaborn line plot
@@ -844,7 +837,6 @@ class FinancialData(object):
         Discounted cash flow (DCF) is a valuation method used to estimate the value of an investment based on its
         expected future cash flows. DCF analysis attempts to figure out the value of an investment today,
         based on projections of how much money it will generate in the future.
-
         :param perpetual_growth_rate: Growth rate of the company after the RRR period : defaulted to 4 %
         :param required_rate_of_return: Required rate of return : Defaulted to 12%
         :param net_profit: If True uses Net Profit for analysis
@@ -941,7 +933,7 @@ class FinancialData(object):
         return value_of_share
         # print(value_of_share, " value per share ")
 
-    def discounted_cash_flow_price_predictor(self, predict_no_yrs=1, plot = False, net_profit=True, operating_cash=False,
+    def discounted_cash_flow_price_predictor(self, predict_no_yrs=1, plot=False, net_profit=True, operating_cash=False,
                                              free_cash=False, required_rate_of_return=0.12, perpetual_growth_rate=0.04):
         """
         :param perpetual_growth_rate: Growth rate of the company after the RRR period : defaulted to 4 %
@@ -953,25 +945,23 @@ class FinancialData(object):
         :return: resturns the fair vale of the stock _ number of years later
         """
 
-        stock_price = self.discounted_cash_flow(using_to_predict=True, p_yrs=predict_no_yrs-1, p_net_profit=net_profit,
-                                         p_operating_cash=operating_cash, p_free_cash=free_cash,
-                                         required_rate_of_return=required_rate_of_return,
-                                         perpetual_growth_rate=perpetual_growth_rate)
+        stock_price = self.discounted_cash_flow(using_to_predict=True, p_yrs=predict_no_yrs, p_net_profit=net_profit,
+                                                p_operating_cash=operating_cash, p_free_cash=free_cash,
+                                                required_rate_of_return=required_rate_of_return,
+                                                perpetual_growth_rate=perpetual_growth_rate)
         if plot:
-            start_date = (datetime.now()) - relativedelta(years=5)
+            start_date = datetime.now() - relativedelta(years=5)
             self.start_input = "{0}-{1}-{2}".format(start_date.year, start_date.month, start_date.day)
-            end_date = datetime.now() - relativedelta(years=1)
-            self.end_input = "{0}-{1}-{2}".format(end_date.year, end_date.month, end_date.day)
 
             tick = "{}.NS".format(self.ticker)
-            stock_price_data = wb.DataReader(tick, data_source='yahoo', start=self.start_input, end=self.end_input)['Adj Close']
+            stock_price_data = wb.DataReader(tick, data_source='yahoo', start=self.start_input)['Adj Close']
             stock_price_data = stock_price_data.to_frame()
             p_date = datetime.now() + relativedelta(years=predict_no_yrs)
             stock_price_data = stock_price_data.reset_index()
             stock_price_data['Date'] = stock_price_data['Date'].apply(lambda x: x.date())
             predict_data = pd.DataFrame(columns=['Date', 'Adj Close'])
-            predict_data.loc[0]=[stock_price_data.iloc[-1]['Date'], stock_price_data.iloc[-1]['Adj Close']]
-            predict_data.loc[1]=[p_date.date(), stock_price]
+            predict_data.loc[0] = [stock_price_data.iloc[-1]['Date'], stock_price_data.iloc[-1]['Adj Close']]
+            predict_data.loc[1] = [p_date.date(), stock_price]
 
             plt.figure(figsize=(15, 8))
             sns.set_style("darkgrid")
@@ -981,7 +971,7 @@ class FinancialData(object):
             plt.tight_layout()
             plt.plot_date(x=predict_data['Date'], y=predict_data['Adj Close'], linestyle='solid', marker=None)
             plt.plot_date(x=stock_price_data['Date'], y=stock_price_data['Adj Close'], linestyle='solid', marker=None)
-            plt.legend(labels=[ "Expectation", 'Declared'])
+            plt.legend(labels=["Expectation", 'Declared'])
             plt.show()
         return stock_price
 
@@ -996,32 +986,31 @@ class FinancialData(object):
             b_data[st] = wb.DataReader(st, data_source='yahoo', start=self.start_input)['Adj Close']
 
         sec_returns = np.log(b_data / b_data.shift(1))
-        cov = sec_returns.cov()*250
+        cov = sec_returns.cov() * 250
         cov_with_nifty = cov.iloc[0, 1]
 
-        nifty_var = sec_returns['^NSEI'].var()*250
+        nifty_var = sec_returns['^NSEI'].var() * 250
 
-        self.st_beta = cov_with_nifty/nifty_var
+        self.st_beta = cov_with_nifty / nifty_var
         return self.st_beta
 
-    def caPM_predict(self, predict_no_yrs=1, risk_free_return=0.04, plot = False):
+    def caPM_predict(self, predict_no_yrs=1, risk_free_return=0.04, plot=False):
         tick = "{}.NS".format(self.ticker)
         stock_price = round(wb.DataReader(tick, data_source='yahoo')['Adj Close'][-1], 2)
         predict_percentage = risk_free_return + self.beta() * 0.05
 
         for y in range(predict_no_yrs):
-            stock_price *= 1+predict_percentage
+            stock_price *= 1 + predict_percentage
 
         if plot:
-
-            stock_price_data = wb.DataReader(tick, data_source='yahoo', start=self.start_input )['Adj Close']
+            stock_price_data = wb.DataReader(tick, data_source='yahoo', start=self.start_input)['Adj Close']
             stock_price_data = stock_price_data.to_frame()
             p_date = datetime.now() + relativedelta(years=predict_no_yrs)
             stock_price_data = stock_price_data.reset_index()
             stock_price_data['Date'] = stock_price_data['Date'].apply(lambda x: x.date())
             predict_data = pd.DataFrame(columns=['Date', 'Adj Close'])
-            predict_data.loc[0]=[stock_price_data.iloc[-1]['Date'],stock_price_data.iloc[-1]['Adj Close']]
-            predict_data.loc[1]=[p_date.date(), stock_price]
+            predict_data.loc[0] = [stock_price_data.iloc[-1]['Date'], stock_price_data.iloc[-1]['Adj Close']]
+            predict_data.loc[1] = [p_date.date(), stock_price]
 
             plt.figure(figsize=(15, 8))
             sns.set_style("darkgrid")
@@ -1031,12 +1020,12 @@ class FinancialData(object):
             plt.tight_layout()
             plt.plot_date(x=predict_data['Date'], y=predict_data['Adj Close'], linestyle='solid', marker=None)
             plt.plot_date(x=stock_price_data['Date'], y=stock_price_data['Adj Close'], linestyle='solid', marker=None)
-            plt.legend(labels=[ "Expectation", 'Declared'])
+            plt.legend(labels=["Expectation", 'Declared'])
             plt.show()
 
         return round(stock_price, 2)
 
-    def plot_stock(self, nifty_50 = True, num_years=5):
+    def plot_stock(self, nifty_50=True, num_years=5):
         tick = "{}.NS".format(self.ticker)
         if nifty_50:
             tickers = [tick, '^NSEI']
@@ -1051,31 +1040,28 @@ class FinancialData(object):
             plot_data[st] = wb.DataReader(st, data_source='yahoo', start=self.start_input)['Adj Close']
 
         plot_data = plot_data.reset_index()
-        plot_data['Date'] = plot_data['Date'].apply(lambda x: x.date())
+        plot_data['index'] = plot_data['index'].apply(lambda x: x.date())
 
         if not nifty_50:
-
             plt.figure(figsize=(15, 8))
             sns.set_style("darkgrid")
             plt.xlabel('Date', fontsize=14)
             plt.ylabel('Price', fontsize=14)
             plt.title("{}".format(tick), fontsize=18)
             plt.tight_layout()
-            plt.plot_date(x=plot_data['Date'], y=plot_data[tick], linestyle='solid', marker=None)
+            plt.plot_date(x=plot_data['index'], y=plot_data[tick], linestyle='solid', marker=None)
             plt.show()
 
         if nifty_50:
-
             log_plt = plot_data
-            log_plt = log_plt.drop('Date', axis=1)
+            log_plt = log_plt.drop('index', axis=1)
             log_plot = (log_plt / log_plt.iloc[0] * 100)
-
 
             plt.figure(figsize=(15, 8))
             sns.set_style("darkgrid")
             plt.xlabel('Time', fontsize=14)
             plt.ylabel('Increase (%)', fontsize=14)
-            plt.title("{} vs Nifty50 \n {} year Chart".format(tick,num_years), fontsize=18)
+            plt.title("{} vs Nifty50 \n {} year Chart".format(tick, num_years), fontsize=18)
             plt.tight_layout()
             plt.xticks([])
             sns.lineplot(data=log_plot, dashes=False)
@@ -1085,4 +1071,5 @@ class FinancialData(object):
 
 if __name__ == '__main__':
     t = FinancialData("tcs")
-    print(t.discounted_cash_flow_price_predictor(predict_no_yrs=1, plot=True))
+    # t.make_predictions(t.net_cash_flow(as_list=True), plot=True)
+    t.plot_stock()
